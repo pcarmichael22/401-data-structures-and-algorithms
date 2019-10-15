b12891c298355f0facba37e2623ace3934a3bfbe
Peter Carmichael <peter.carmichael22@gmail.com>
2019-10-15 15:42:11 -0700

    Code Challenge 1 complete

---
+++ arrayReverse/array-reverse.js
@@ -0,0 +1,12 @@
+const inputArr = [1,2,3,4,5,6,7];
+
+function reverse(array){
+const outputArr = [];
+while(array.length){
+    outputArr.push(array.pop());
+    }
+}
+
+return(reverse(inputArr));
+
+
